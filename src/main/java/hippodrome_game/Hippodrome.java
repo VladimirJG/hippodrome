ea0fcf56ef8719d8ca48ff85a35a9c5d9ce24914
VladimirJG <vladimi85@gmail.com>
2023-04-19 09:43:19 +0300

    added list of horses

--- src/main/java/hippodrome_game/Hippodrome.java
+++ src/main/java/hippodrome_game/Hippodrome.java
@@ -1,10 +1,17 @@
 package hippodrome_game;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
     public static void main(String[] args) {
+        List<Horse> horseList = new ArrayList<>();
+        horseList.add(new Horse("horse1", 3, 0));
+        horseList.add(new Horse("horse2", 3, 0));
+        horseList.add(new Horse("horse3", 3, 0));
 
+        Hippodrome hippodrome = new Hippodrome(horseList);
+        game = hippodrome;
     }
 
     private List<Horse> horses;
@@ -16,4 +23,6 @@ public class Hippodrome {
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
     }
+
+    static Hippodrome game;
 }
